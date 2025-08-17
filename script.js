89a6c37eee9ad2ef104e199fed43375e021ce835
GamerBoyBK417 <bibekmondal71000@gmail.com>
2025-08-18 00:30:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const statusMessage = document.getElementById('statusMessage');
 
 // !! IMPORTANT !!
 // PASTE YOUR DISCORD WEBHOOK URL HERE
-const DISCORD_WEBHOOK_URL = 'YOUR_WEBHOOK_URL_HERE';
+const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1406714029048856656/rcCH-OCTQ8YA5sljAtwWo4cIfvfs8nky_gxK8Jow65VvQueCBZXtB2xoDRHX0NLvuFbK';
 
 form.addEventListener('submit', function(event) {
     // Prevent the default form submission behavior
